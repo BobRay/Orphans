c6e464f3ba6b8afbc3a44da8ccc0603446b70a53
bobray <bobray@softville.com>
2013-05-02 16:19:41 -0500

    remove dead code and lex strings

--- core/components/orphans/model/orphans/orphans.class.php
+++ core/components/orphans/model/orphans/orphans.class.php
@@ -149,9 +149,6 @@ class Orphans {
         return $this->output;
     }
 
-   /* public function getObjects($type) {
-        return $this->allObjects[$type];
-    }*/
 
     public function getNameAlias($type) {
         switch ($type) {
@@ -509,50 +506,4 @@ class Orphans {
         return true;
     }
 
-    /**
-     * Gets a Chunk and caches it; also falls back to file-based templates
-     * for easier debugging.
-     *
-     * @access public
-     * @param string $name The name of the Chunk
-     * @param array $properties The properties for the Chunk
-     * @return string The processed content of the Chunk
-     */
-    /*public function getChunk($name,$properties = array()) {
-        $chunk = null;
-        if (!isset($this->chunks[$name])) {
-            $chunk = $this->_getTplChunk($name);
-            if (empty($chunk)) {
-                $chunk = $this->modx->getObject('modChunk',array('name' => $name),true);
-                if ($chunk == false) return false;
-            }
-            $this->chunks[$name] = $chunk->getContent();
-        } else {
-            $o = $this->chunks[$name];
-            $chunk = $this->modx->newObject('modChunk');
-            $chunk->setContent($o);
-        }
-        $chunk->setCacheable(false);
-        return $chunk->process($properties);
-    }*/
-
-    /**
-     * Returns a modChunk object from a template file.
-     *
-     * @access private
-     * @param string $name The name of the Chunk. Will parse to name.chunk.tpl
-     * @return modChunk/boolean Returns the modChunk object if found, otherwise
-     * false.
-     */
-/*    private function _getTplChunk($name) {
-        $chunk = false;
-        $f = $this->config['chunksPath'].strtolower($name).'.chunk.tpl';
-        if (file_exists($f)) {
-            $o = file_get_contents($f);
-            $chunk = $this->modx->newObject('modChunk');
-            $chunk->set('name',$name);
-            $chunk->setContent($o);
-        }
-        return $chunk;
-    }*/
 }
